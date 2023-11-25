29034553ad1378f24f6722f91f944e51e76e32b2
Peng wang <peng.wang@bytedance.com>
2023-11-24 18:09:41 -0800

    update util

--- extern/ImageDream/imagedream/ldm/util.py
+++ extern/ImageDream/imagedream/ldm/util.py
@@ -1,5 +1,6 @@
 import importlib
 
+import random
 import torch
 import numpy as np
 from collections import abc
@@ -210,3 +211,17 @@ def parallel_data_prefetch(
         return out
     else:
         return gather_res
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+def add_random_background(image, bg_color=None):
+    bg_color = np.random.rand() * 255 if bg_color is None else bg_color
+    image = np.array(image)
+    rgb, alpha = image[..., :3], image[..., 3:]
+    alpha = alpha.astype(np.float32) / 255.0
+    image_new = rgb * alpha + bg_color * (1 - alpha)
+    return Image.fromarray(image_new.astype(np.uint8))
