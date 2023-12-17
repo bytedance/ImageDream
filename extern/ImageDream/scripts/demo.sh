320d379528dcae4968e8a5af82e2ed3874482722
peng-wang <146033206+raptor-wang@users.noreply.github.com>
2023-12-17 15:30:57 -0800

    Update demo.sh

--- extern/ImageDream/scripts/demo.sh
+++ extern/ImageDream/scripts/demo.sh
@@ -1,18 +1,18 @@
 
-# # test pixel version
-# python scripts/demo.py  \
-#     --image "./assets/astronaut.png" \
-#     --text "an astronaut riding a horse" \
-#     --config_path "./imagedream/configs/sd_v2_base_ipmv.yaml" \
-#     --ckpt_path "./release_models/sd-v2.1-base-4view-ipmv.pt" \
-#     --mode "pixel"
-#     --num_frames 5
+# test pixel version
+python scripts/demo.py  \
+    --image "./assets/astronaut.png" \
+    --text "an astronaut riding a horse" \
+    --config_path "./imagedream/configs/sd_v2_base_ipmv.yaml" \
+    --ckpt_path "./release_models/ImageDream/sd-v2.1-base-4view-ipmv.pt" \
+    --mode "pixel"
+    --num_frames 5
 
 # test local version
 python scripts/demo.py  \
     --image "./assets/astronaut.png" \
     --text "an astronaut riding a horse" \
     --config_path "./imagedream/configs/sd_v2_base_ipmv_local.yaml" \
-    --ckpt_path "./release_models/sd-v2.1-base-4view-ipmv-local.pt" \
+    --ckpt_path "./release_models/ImageDream/sd-v2.1-base-4view-ipmv-local.pt" \
     --mode "local" \
     --num_frames 4
